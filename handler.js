93abb48540ac185c6adf9a6e076e3697b673f873
Michiel de Jong <michiel@unhosted.org>
2011-12-11 20:33:57 +0700

    protect against missing ?userAddress=...@... param

--- handler.js
+++ handler.js
@@ -11,19 +11,23 @@ exports.handler = (function() {
         res.writeHead(200, {
           'Content-Type': 'xrd+xml',
           'Access-Control-Allow-Origin': '*'});
-        var userAddressParts = (urlObj.query.userAddress.split('@'));
-        while(userAddressParts.length <2) {//avoid breaking errors
-          userAddressParts.push(undefined);
-        }
-        res.end('<?xml version="1.0" encoding="UTF-8"?>\n'
-          +'<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">\n'
-	        +'  <hm:Host xmlns="http://host-meta.net/xrd/1.0">'+userAddressParts[1]+'</hm:Host>\n'
-	        +'  <Link rel="remoteStorage"\n'
-          +'    template="http://'+config.subdomainCouchHosting+'/CouchDB/proxy/'+userAddressParts[0]+'.'+userAddressParts[1]+'/{category}/"\n'
-          +'    auth="http://'+config.subdomainCouchHosting+'/CouchDB/auth/'+userAddressParts[0]+'.'+userAddressParts[1]+'"\n'
-          +'    api="CouchDB"\n'
-          +'  ></Link>\n'
-          +'</XRD>\n');
+        if(!urlObj.query.userAddress) {
+          res.end('please specify ?userAddress=...@...');
+        } else {
+          var userAddressParts = (urlObj.query.userAddress.split('@'));
+          while(userAddressParts.length <2) {//avoid breaking errors
+            userAddressParts.push(undefined);
+          }
+          res.end('<?xml version="1.0" encoding="UTF-8"?>\n'
+            +'<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">\n'
+            +'  <hm:Host xmlns="http://host-meta.net/xrd/1.0">'+userAddressParts[1]+'</hm:Host>\n'
+            +'  <Link rel="remoteStorage"\n'
+            +'    template="http://'+config.subdomainCouchHosting+'/CouchDB/proxy/'+userAddressParts[1]+'/{category}/"\n'
+            +'    auth="http://'+config.subdomainCouchHosting+'/CouchDB/auth/'+userAddressParts[1]+'/'+userAddressParts[0]+'"\n'
+            +'    api="CouchDB"\n'
+            +'  ></Link>\n'
+            +'</XRD>\n');
+         }
       } else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('Not found\n');
