79678b9fb187363ae18d09e39fa569691ba99112
Michiel de Jong <michiel@unhosted.org>
2012-01-05 15:04:50 +0700

    advancing usersDb

--- handler.js
+++ handler.js
@@ -1,120 +1,139 @@
 exports.handler = (function() {
-  var http = require('http'),
-    url = require('url'),
-    config = require('./config').config;
+  var url = require('url'),
+    https = require('https'),
+    querystring = require('querystring'),
+    browseridVerify = require('browserid-verifier'),
+    fs = require('fs'),
+    userDb = require('./config').config;
 
-  function serve(req, res) {
-  //user.unhosted.org/ handle POST with params:
-  //  - assertion
-  //  - audience
-  //
-  //  - returns either:
-  //     ok: true
-  //     email: ...
-  //     remoteStorage:
-  //       template:
-  //       api:
-  //       auth:
-  //       browseridaccess:
-  //  - or:
-  //     email: ...
-  //     token: ...
-  //
-  //    and db will contain: email -> {token: ..., state:needed}
-  //user.unhosted.org/pimper handle POST with params:
-  //  - action: provision
-  //  - token
-  //  - email
-  //  - firstName
-  //  - lastName
-  //  - userName
-  //
-  //    and db will contain: email -> {token: ..., address: 'http://'+userName+'.iriscouch.com:5984/', state:virgin}
-  //
-  //or:
-  //  - action: squat
-  //  - token
-  //  - address
-  //  - email
-  //
-  //    and db will contain: email -> {token: ..., address: 'http://'+userName+'.iriscouch.com:5984/', state:squatted, adminPwd:...}
-  //
-  //or:
-  //  - action: createDb
-  //  - token
-  //  - userName
-  //  - email
-  //
-  //user.unhosted.org/BrowserId2IrisCouch handle POST with params:
-  //  - assertion
-  //  - audience
-  //
-  // response: a token for this audience.
-  //
-      var urlObj = url.parse(req.url, true);
-      console.log(urlObj);
-      if(urlObj.pathname == '/') {
-        if(req.method=='GET') {
-          res.writeHead(200);
-          res.end('<!DOCTYPE html><html lang="en"><head><title>Linking your user address to your lrdd file in a centralized way...</title><meta charset="utf-8"></head><body>'
-            +'Loading, please wait...'
-            +'<script src="http://browserid.org/include.js"></script>'
-            +'<script>document.write(\'<input type="submit" value="Click me" onclick="go();">\');'
-            +'function go() {'
-            +'  navigator.id.get(function(assertion) {'
-            +'    var xhr=new XMLHttpRequest();xhr.open(\'POST\', \'\', false);xhr.send(JSON.stringify({'
-            +'      browserIdAssertion: assertion,'
-            +'      lrdd: \''+urlObj.query.set_lrdd+'\''
-            +'    }));'
-            +'    window.location = \''+urlObj.query.assertion_also_to+'?assertion=\'+assertion'
-            +'  }, {requiredEmail: \''+urlObj.query.userAddress+'\'});'
-            +'}'
-            +'</script>'
-            +'</body></html>');
-        } else if(req.method == 'POST') {
-          console.log("It's a POST");
-          var dataStr = '';
-          req.on('data', function(chunk) { dataStr += chunk; });
-          req.on('end', function() {
-            var dataObj = JSON.parse(dataStr);
-            console.log(dataObj);
-            res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': dataObj.audience});
-            res.write(JSON.stringify({
-                email: 'dejong.michiel@gmail.com',
-                token: 'asdf'
-              })
-            );
-            res.end();
-          });
-        }
-      } else if(false) {
-        res.writeHead(200, {
-          'Content-Type': 'xrd+xml',
-          'Access-Control-Allow-Origin': '*'});
-        if(!urlObj.query.userAddress) {
-          res.end('please specify ?userAddress=...@...');
-        } else {
-          var userAddressParts = (urlObj.query.userAddress.split('@'));
-          while(userAddressParts.length <2) {//avoid breaking errors
-            userAddressParts.push(undefined);
-          }
-          res.end('<?xml version="1.0" encoding="UTF-8"?>\n'
-            +'<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">\n'
-            +'  <hm:Host xmlns="http://host-meta.net/xrd/1.0">'+userAddressParts[1]+'</hm:Host>\n'
-            +'  <Link rel="remoteStorage"\n'
-            +'    template="http://'+config.subdomainCouchHosting+'/CouchDB/proxy/'+userAddressParts[1]+'/{category}/"\n'
-            +'    auth="http://'+config.subdomainCouchHosting+'/CouchDB/auth/'+userAddressParts[1]+'/'+userAddressParts[0]+'"\n'
-            +'    api="CouchDB"\n'
-            +'  ></Link>\n'
-            +'</XRD>\n');
-         }
-      } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not Found\n');
-      }
+  function randStr(length) {
+    var buffer = new Buffer(length);
+    var fd = fs.openSync('/dev/urandom', 'r');
+    fs.readSync(fd, buffer, 0, buffer.length, 0);
+    fs.closeSync(fd);
+    var randStr = buffer.toString('base64');
+    console.log('generated randStr of length '+randStr.length+' ('+length+'):'+randStr);
+    return randStr;
   }
+  function webfingerLookup(userAddress, origin, res) {
+    var userAddressParts = userAddress.split('@');
+    var options = {
+      host: userAddressParts[1],
+      port: 443,
+      path: '/.well-known/host-meta',
+      method: 'GET'
+    }
+    //var request = https.request(options, function(response) {
+    //  if(response.statusCode == 200) {
+    //    //parse the xml
+    //  } else {
+        var authStr = userDb.usr + ':' + userDb.pwd;
+        var options2 = {
+          host: userDb.host,
+          port: 443,
+          path: '/'+userDb.dbName+'/'+userAddress,
+          method: 'PUT',
+          headers: {
+            'Authorization': 'Basic ' + new Buffer(authStr).toString('base64')
+          }
+        };
+        console.log(options);
+        var token = randStr(40);
+        var data= {
+          token: token
+        }
+        res.writeHead(200, {
+          'Content-type': 'application/json',
+          'Access-Control-Allow-Origin': origin
+        });
+        res.write(JSON.stringify(data));
+        res.end();
+        var request2 = https.request(options2, function(response2) {
+          console.log('STATUS: ' + response2.statusCode);
+          response2.setEncoding('utf8');
+          var resStr = '';
+          response2.on('data', function (chunk) {
+            resStr += chunk;
+            console.log('BODY2: ' + chunk);
+          });
+          response2.on('end', function() {
+            console.log('END2');
+          });
+        });
+        request2.write(JSON.stringify(data));
+        request2.end();
+    //  }
+    //});
+  }
+  function serve(req, res, baseDir) {
+    var dataStr = '';
+    req.on('data', function(chunk) {
+      dataStr += chunk;
+    });
+    req.on('end', function() {
+      var incoming = JSON.parse(dataStr);
+      console.log(incoming);
+      postData = {
+        audience: 'http://libredocs.org',
+        assertion: incoming.assertion
+      };
+      console.log(postData);
+      browseridVerify(postData, function(err, r) {
+        if(err) {
+          res.writeHead(200, {'Content-type': 'application/json'});
+          res.write(JSON.stringify(err));
+          res.end();
+        } else {
+          if(r.email) {
+            console.log(r.email+' confirmed by browserid verifier');
+            var authStr = userDb.usr + ':' + userDb.pwd;
+            console.log(authStr);
+            var options = {
+              host: userDb.host,
+              port: 443,
+              path: '/'+userDb.dbName+'/'+r.email,
+              method: 'GET', 
+              headers: {
+                'Authorization': 'Basic ' + new Buffer(authStr).toString('base64')
+              }
+            };
+            console.log(options);
+            var request = https.request(options, function(response) {
+              console.log('STATUS: ' + response.statusCode);
+              console.log('HEADERS: ' + JSON.stringify(response.headers));
+              response.setEncoding('utf8');
+              var resStr = '';
+              response.on('data', function (chunk) {
+                resStr += chunk;
+                console.log('BODY: ' + chunk);
+              });
+              response.on('end', function() {
+                if(response.statusCode == 404) {
+                  webfingerLookup(r.email, 'http://libredocs.org', res);
+                } else {
+                  console.log('END; writing to res: "'+resStr+'"');
+                  res.writeHead(response.statusCode, response.headers);
+                  res.write('hi');
+                  res.end(resStr);
+                }
+              });
+            });
+            //console.log('writing to the request');
+
+            //var data = JSON.stringify({
+            //});
+            //console.log(data);
+            //request.write(data);
+            console.log('ending the request');
+            request.end();
+            console.log('setting request.on(\'response\', ...)');
+          }
+        }
+      });
+    });
+  }
+
   return {
     serve: serve
-  }
-
+  };
 })();
