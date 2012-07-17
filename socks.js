30bbaeec3ae357bca3c5fc12bb878f1c00f45915
Michiel de Jong <michiel@unhosted.org>
2012-07-17 13:41:46 +0200

    starting to add sockets

---
+++ socks.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var sockjs = require('sockjs');
+
+var echo = sockjs.createServer();
+echo.on('connection', function(conn) {
+    conn.on('data', function(message) {
+        conn.write(message);
+    });
+    conn.on('close', function() {});
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
