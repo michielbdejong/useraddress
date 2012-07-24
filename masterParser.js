d08d0ee1b42cd3aa343339923fe45674566e252f
Michiel de Jong <michiel@unhosted.org>
2012-07-24 18:01:46 +0200

    trying to put the Content-Types into masterParser

--- masterParser.js
+++ masterParser.js
@@ -56,7 +56,13 @@ function doParse(content, language, identifiers, cb) {
 function fetch(urlStr, cb) {
   //console.log(urlStr);
   if(urlStr.substring(0, 'file://exampleFiles/'.length) == 'file://exampleFiles/') {
-    fs.readFile(urlStr.substring('file://'.length), cb);
+    fs.readFile(urlStr.substring('file://'.length), function(err, content) {
+      var urlStrParts = urlStr.split('.');
+      cb(err, {
+        content: content,
+        type: urlStrParts[urlStrParts.length-1]//html, xrd, rdf, js, json
+      });
+    });
   } else {
     console.log('********* CACHE MISS: '+urlStr);
     var urlObj = url.parse(urlStr);
@@ -81,7 +87,10 @@ function fetch(urlStr, cb) {
         //console.log('got end');
         if(timer) {//not timed out yet
           if(response.statusCode==200 || response.statusCode==201 || response.statusCode==204) {
-            cb(null, str);
+            cb(null, {
+              content: str,
+              type: response.headers['Content-Type'] 
+            });
           } else if(response.statusCode==301 || response.statusCode==302) {
             fetch(response.headers.location, cb);
           } else {
@@ -115,59 +124,59 @@ function fetch(urlStr, cb) {
 }
 function checkStubs(url) {
   var cache = {
-   'https://identi.ca/.well-known/host-meta?resource=acct:michielbdejong@identi.ca': 'file://exampleFiles/id-hostmeta',
-   'http://identi.ca/main/xrd?uri=acct:michielbdejong@identi.ca': 'file://exampleFiles/id-lrdd',
-   'http://identi.ca/michielbdejong/foaf': 'file://exampleFiles/id-foaf',
+   'https://identi.ca/.well-known/host-meta?resource=acct:michielbdejong@identi.ca': 'file://exampleFiles/id-hostmeta.xrd',
+   'http://identi.ca/main/xrd?uri=acct:michielbdejong@identi.ca': 'file://exampleFiles/id-lrdd.xrd',
+   'http://identi.ca/michielbdejong/foaf': 'file://exampleFiles/id-foaf.html',
 
-   'https://gmail.com/.well-known/host-meta?resource=acct:dejong.michiel@gmail.com': 'file://exampleFiles/gm-hostmeta',
-   'http://www.google.com/s2/webfinger/?q=acct:dejong.michiel@gmail.com': 'file://exampleFiles/gm-lrdd',
-   'http://www.google.com/s2/webfinger/?q=acct%3Adejong.michiel%40gmail.com&fmt=foaf': 'file://exampleFiles/gm-foaf',
-   'http://www-opensocial.googleusercontent.com/api/people/108912615873187638071/': 'file://exampleFiles/gm-poco-me',
-   'http://www.google.com/profiles/dejong.michiel': 'file://exampleFiles/gm-hcard',
+   'https://gmail.com/.well-known/host-meta?resource=acct:dejong.michiel@gmail.com': 'file://exampleFiles/gm-hostmeta.xrd',
+   'http://www.google.com/s2/webfinger/?q=acct:dejong.michiel@gmail.com': 'file://exampleFiles/gm-lrdd.xrd',
+   'http://www.google.com/s2/webfinger/?q=acct%3Adejong.michiel%40gmail.com&fmt=foaf': 'file://exampleFiles/gm-foaf.html',
+   'http://www-opensocial.googleusercontent.com/api/people/108912615873187638071/': 'file://exampleFiles/gm-poco-me.html',
+   'http://www.google.com/profiles/dejong.michiel': 'file://exampleFiles/gm-hcard.html',
 
-   'https://revolutionari.es/.well-known/host-meta?resource=acct:michiel@revolutionari.es': 'file://exampleFiles/fr-hostmeta',
-   'https://revolutionari.es/xrd/?uri=acct:michiel@revolutionari.es': 'file://exampleFiles/fr-lrdd',
-   'https://revolutionari.es/poco/michiel': 'file://exampleFiles/fr-poco',
-   'https://revolutionari.es/hcard/michiel': 'file://exampleFiles/fr-hcard',
+   'https://revolutionari.es/.well-known/host-meta?resource=acct:michiel@revolutionari.es': 'file://exampleFiles/fr-hostmeta.xrd',
+   'https://revolutionari.es/xrd/?uri=acct:michiel@revolutionari.es': 'file://exampleFiles/fr-lrdd.xrd',
+   'https://revolutionari.es/poco/michiel': 'file://exampleFiles/fr-poco.html',
+   'https://revolutionari.es/hcard/michiel': 'file://exampleFiles/fr-hcard.html',
    
-   'https://joindiaspora.com/.well-known/host-meta?resource=acct:michielbdejong@joindiaspora.com': 'file://exampleFiles/jd-hostmeta',
-   'https://joindiaspora.com/webfinger?q=acct:michielbdejong@joindiaspora.com': 'file://exampleFiles/jd-lrdd',
-   'https://joindiaspora.com/hcard/users/e583028f23ce0302': 'file://exampleFiles/jd-hcard',
+   'https://joindiaspora.com/.well-known/host-meta?resource=acct:michielbdejong@joindiaspora.com': 'file://exampleFiles/jd-hostmeta.xrd',
+   'https://joindiaspora.com/webfinger?q=acct:michielbdejong@joindiaspora.com': 'file://exampleFiles/jd-lrdd.xrd',
+   'https://joindiaspora.com/hcard/users/e583028f23ce0302': 'file://exampleFiles/jd-hcard.html',
 
-   'https://api.twitter.com/1/users/show.json?screen_name=michielbdejong': 'file://exampleFiles/twitter-api', 
-   'https://graph.facebook.com/dejong.michiel': 'file://exampleFiles/fb-api', 
-   'http://melvincarvalho.com/': 'file://exampleFiles/melvin-html', 
+   'https://api.twitter.com/1/users/show.json?screen_name=michielbdejong': 'file://exampleFiles/twitter-api.json',
+   'https://graph.facebook.com/dejong.michiel': 'file://exampleFiles/fb-api.js',
+   'http://melvincarvalho.com/': 'file://exampleFiles/melvin.html',
    
-   'http://www.w3.org/People/Berners-Lee/card.rdf': 'file://exampleFiles/timbl-foaf',
-   'http://graph.facebook.com/512908782': 'file://exampleFiles/timbl-fb',
-   'http://identi.ca/user/45563': 'file://exampleFiles/timbl-id',
-   'http://www.advogato.org/person/timbl/foaf.rdf': 'file://exampleFiles/timbl-foaf2',
-   'http://www4.wiwiss.fu-berlin.de/bookmashup/persons/Tim+Berners-Lee': 'file://exampleFiles/timbl-fu1',
-   'http://www4.wiwiss.fu-berlin.de/dblp/resource/person/100007': 'file://exampleFiles/timbl-fu2',
+   'http://www.w3.org/People/Berners-Lee/card.rdf': 'file://exampleFiles/timbl-foaf.html',
+   'http://graph.facebook.com/512908782': 'file://exampleFiles/timbl-fb.html',
+   'http://identi.ca/user/45563': 'file://exampleFiles/timbl-id.html',
+   'http://www.advogato.org/person/timbl/foaf.rdf': 'file://exampleFiles/timbl-foaf2.html',
+   'http://www4.wiwiss.fu-berlin.de/bookmashup/persons/Tim+Berners-Lee': 'file://exampleFiles/timbl-fu1.html',
+   'http://www4.wiwiss.fu-berlin.de/dblp/resource/person/100007': 'file://exampleFiles/timbl-fu2.html',
 
-   'http://tantek.com/': 'file://exampleFiles/tantek-html',
-   'http://www.facebook.com/tantek.celik': 'file://exampleFiles/tantek-fb',
-   'http://twitter.com/t': 'file://exampleFiles/tantek-twitter',
-   'http://flickr.com/tantek/': 'file://exampleFiles/tantek-flickr',
+   'http://tantek.com/': 'file://exampleFiles/tantek.html',
+   'http://www.facebook.com/tantek.celik': 'file://exampleFiles/tantek-fb.html',
+   'http://twitter.com/t': 'file://exampleFiles/tantek-twitter.html',
+   'http://flickr.com/tantek/': 'file://exampleFiles/tantek-flickr.html',
    
-   'http://last.fm/user/tantekc': 'file://exampleFiles/tantek-lastfm',
-   'http://www.last.fm/user/tantekc': 'file://exampleFiles/tantek-lastfm',
+   'http://last.fm/user/tantekc': 'file://exampleFiles/tantek-lastfm.html',
+   'http://www.last.fm/user/tantekc': 'file://exampleFiles/tantek-lastfm.html',
 
-   'http://plancast.com/t': 'file://exampleFiles/tantek-plancast',
+   'http://plancast.com/t': 'file://exampleFiles/tantek-plancast.html',
    
-   'http://upcoming.yahoo.com/user/6623': 'file://exampleFiles/tantek-yahoo',
+   'http://upcoming.yahoo.com/user/6623': 'file://exampleFiles/tantek-yahoo.html',
    
-   'http://lanyrd.com/people/t': 'file://exampleFiles/tantek-lanyrd',
-   'http://lanyrd.com/profile/t': 'file://exampleFiles/tantek-lanyrd2',
-   'http://lanyrd.com/profile/t/': 'file://exampleFiles/tantek-lanyrd3',
+   'http://lanyrd.com/people/t': 'file://exampleFiles/tantek-lanyrd.html',
+   'http://lanyrd.com/profile/t': 'file://exampleFiles/tantek-lanyrd2.html',
+   'http://lanyrd.com/profile/t/': 'file://exampleFiles/tantek-lanyrd3.html',
    
-   'http://foursquare.com/t': 'file://exampleFiles/tantek-foursquare',
-   'https://foursquare.com/t': 'file://exampleFiles/tantek-foursquare2',
+   'http://foursquare.com/t': 'file://exampleFiles/tantek-foursquare.html',
+   'https://foursquare.com/t': 'file://exampleFiles/tantek-foursquare2.html',
    
-   'http://google.com/profiles/tantek': 'file://exampleFiles/tantek-google',
-   'http://www.google.com/profiles/tantek': 'file://exampleFiles/tantek-google2',
-   'https://profiles.google.com/tantek': 'file://exampleFiles/tantek-google3',
-   'https://plus.google.com/109182513536739786206': 'file://exampleFiles/tantek-google4',
+   'http://google.com/profiles/tantek': 'file://exampleFiles/tantek-google.html',
+   'http://www.google.com/profiles/tantek': 'file://exampleFiles/tantek-google2.html',
+   'https://profiles.google.com/tantek': 'file://exampleFiles/tantek-google3.html',
+   'https://plus.google.com/109182513536739786206': 'file://exampleFiles/tantek-google4.html',
 }
 
   if(cache[url.split('#')[0]]) {
@@ -190,23 +199,20 @@ function parse(url, docRel, identifiers, cb) {
       //console.log('fetch error '+url+' '+err);
       cb(err);
     } else {
-      var parsed;
-      try {
-        parsed = JSON.parse(data);
-      } catch(e) {//JSON failed, try xml
+      if(data.type=='js' || data.type=='json') {
+        var parsed;
+        try {
+          parsed = JSON.parse(data);
+          if(parsed) {
+            doParse(parsed, docRel, identifiers, cb);
+          }
+        } catch(e) {//JSON failed, try xml
+          cb('mime type was '+data.type+' but no valid JSON');
+        }
+      } else if(data.type=='xml' || data.type=='xrd' || data.type=='rdf') {
         new xml2js.Parser().parseString(data, function(err, data2) {
           if(err || data2==null) {//XML failed, try html
-            var handler = new htmlparser.DefaultHandler(function (err, data3) {
-              if(err) {
-               //console.log('handler error '+url);
-               cb(err);
-              } else {
-                //console.log('doParse '+url);
-                doParse(data3, docRel, identifiers, cb);
-              }
-            });
-            var parser = new htmlparser.Parser(handler);
-            parser.parseComplete(data);
+            cb('mime type was '+data.type+' but no valid XML');
           } else {
             if(docRel=='lrdd' && data2['@'] && data2['@'].xmlns && data2['@'].xmlns == 'http://docs.oasis-open.org/ns/xri/xrd-1.0') {
               //console.log('doParse '+url);
@@ -225,12 +231,18 @@ function parse(url, docRel, identifiers, cb) {
           }
         });
         return;
-      }
-      if(parsed) {
-        doParse(parsed, docRel, identifiers, cb);
+      } else if(data.type=='html') {
+        var handler = new htmlparser.DefaultHandler(function (err, data3) {
+          if(err) {
+            cb('mime type was '+data.type+' but no valid HTML');
+          } else {
+            doParse(data3, docRel, identifiers, cb);
+          }
+        });
+        var parser = new htmlparser.Parser(handler);
+        parser.parseComplete(data);
       } else {
-        //console.log('no idea '+url);
-        cb('no idea what this is');
+        cb('no idea what to do with Content-Type '+data.type);
       }
     }
   });
